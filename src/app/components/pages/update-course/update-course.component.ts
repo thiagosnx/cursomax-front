af9c7a6d7ee9f1495dfc37fe0aff7d5b0f1bf807
Thiago Santana Cerqueira <T1092125@interno.bb.com.br>
2025-02-04 14:26:20 -0300

    fix: update reativo

--- src/app/components/pages/update-course/update-course.component.ts
+++ src/app/components/pages/update-course/update-course.component.ts
@@ -3,6 +3,8 @@ import { Course } from '../../../models/Course';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseService } from '../../../services/course.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-update-course',
@@ -10,39 +12,84 @@ import { CourseService } from '../../../services/course.service';
   styleUrl: './update-course.component.css'
 })
 export class UpdateCourseComponent implements OnInit {
-  course: Course = {
-    "id": 0,
-    "ttl": "",
-    "dt_lcmt": "",
-    "dett":"",
-    "cghr": "",
-    "img": "",
-    "url": "",
-    "tip": 1,
-    "ntc": true,
-    "emp": "",
-  }
+  
+  form!: FormGroup;
   constructor(
-    private http: HttpClient,
     private router: Router,
     private route: ActivatedRoute,
     private service: CourseService,
+    private formBuilder: FormBuilder,
   ){ }
-
+  courseId = 0;
   ngOnInit(): void {
-      const id = this.route.snapshot.paramMap.get("id");
+    this.form = this.formBuilder.group({
+      id: '',
+      ttl : '',
+      dt_lcmt: '',
+      dett: '',
+      cghr: '',
+      img: '',
+      url: '',
+      tip: '',
+      ntc: '',
+      emp: ''
+    });
+    const id = this.route.snapshot.paramMap.get('id') 
+    this.courseId = parseInt(id!);
       this.service.getCourseById(parseInt(id!)).subscribe((course) => {
-        this.course = course;
+        this.form = this.formBuilder.group({
+          id:[course.id],
+            
+          ttl: [course.ttl, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+          dt_lcmt: [course.dt_lcmt, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+          dett: [course.dett, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+          cghr: [course.cghr, Validators.compose([
+            Validators.required,
+            Validators.minLength(1)
+          ])],
+          img: [course.img, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+          url: [course.url, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+          tip: [course.tip, Validators.compose([
+            Validators.required,
+            Validators.minLength(1)
+          ])],
+          ntc: [course.ntc, Validators.compose([
+            Validators.required,
+            Validators.minLength(1)
+          ])],
+          emp: [course.emp, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+        })
       })
+      
   }
 
   updateCourse(){
-    this.service.updateCourse(this.course).subscribe(()=>{
-      this.router.navigate(['/cursos']);
-    })
+    if(this.form.valid){
+      this.service.updateCourse(this.form.value).subscribe(()=>{
+        this.router.navigate(['/cursos']);
+      })
+    }
   }
 
   cancel(){
-    this.router.navigate([`/curso/${this.course.id}`]);
+    this.router.navigate([`/curso/${this.courseId}`]);
   }
 }
